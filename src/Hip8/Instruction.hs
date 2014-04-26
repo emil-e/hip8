7ba41061efd4416614f04bbd1e42977210c73904
Emil Eriksson <shadewind@gmail.com>
2014-04-26 12:28:06 +0200

    All instructions parsed

--- src/Hip8/Instruction.hs
+++ src/Hip8/Instruction.hs
@@ -11,19 +11,47 @@ module Hip8.Instruction (
   SystemTransformer,
   InstructionInfo(..),
   Instruction(..),
-  showInstruction,
-  binToString,
   parseInstruction,
   sys,
   cls,
-  ret
+  ret,
+  jpAddr,
+  callAddr,
+  seRegByte,
+  sneRegByte,
+  seRegReg,
+  ldRegByte,
+  addRegByte,
+  ldRegReg,
+  orRegReg,
+  andRegReg,
+  xorRegReg,
+  addRegReg,
+  subRegReg,
+  shr,
+  subnRegReg,
+  shl,
+  sneRegReg,
+  ldIAddr,
+  jpV0Addr,
+  rnd,
+  drw,
+  skp,
+  sknp,
+  ldRegDT,
+  ldRegKey,
+  ldDTReg,
+  ldSTReg,
+  addIReg,
+  ldFReg,
+  ldBReg,
+  ldMemRegs,
+  ldRegsMem
   ) where
 
 import Hip8.BitParser
 import Control.Applicative
 import Data.Word
-import Data.List
-import Data.Maybe
 
 -- |The input environment for the emulation
 type Environment = ()
@@ -37,48 +65,82 @@ newtype CpuException = CpuException String
 -- |Function which transforms the state of the system given an environment.
 type SystemTransformer = Environment -> SystemState -> Either CpuException SystemState
 
--- |Describes the name and arguments of an instruction
-data InstructionInfo = InstructionInfo String [Word16] deriving (Show, Eq, Ord)
+-- |A pseudo-argument to an instruction.
+data PseudoArg = Addr Word16
+               | Byte Word16
+               | Nibble Word16
+               | Reg Word16
+               | RegI
+               | AddrI
+               | DelayTimer
+               | SoundTimer
+               | SpriteLoc
+               | BCD
+               | Key
+               deriving (Show, Eq)
+                      
+-- |Information about an instruction.
+data InstructionInfo = InstructionInfo String [PseudoArg]
+                     deriving (Show, Eq)
 
--- |An instruction with info and a 'SystemTransformer' which performs the operation
--- described by the info.
+-- |An instruction with a describing string and a 'SystemTransformer' which performs
+-- the operation described by the info.
 data Instruction = Instruction InstructionInfo SystemTransformer
 
--- |Calls 'showInfo' on the info of the 'Instruction'
-showInstruction :: Instruction -> String
-showInstruction (Instruction info _) = showInfo info
+instance Show Instruction where
+  show (Instruction info _) = show info
 
--- |Returns a string describing the instruction of the form "<NAME> <ARG>, <ARGS>, ..."
-showInfo :: InstructionInfo -> String
-showInfo (InstructionInfo name []) = name
-showInfo (InstructionInfo name args) = name ++ " " ++ intercalate ", " (map show args)
-
--- |Parses the given binary instruction to a string representation.
-binToString :: Word16 -> String
-binToString word = fromMaybe "" (showInstruction <$> parseInstruction word)
+instance Eq Instruction where
+  (Instruction a _) == (Instruction b _) = a == b
 
 parseInstruction :: Word16 -> Maybe Instruction
 parseInstruction word =
   runParser 16 word 
-     $  const16 0x00E0 *> pure cls 
-    <|> const16 0x00EE *> pure ret 
+     $  const16 0x00E0 *> pure cls
+    <|> const16 0x00EE *> pure ret
     <|> const4 0x0 *> (sys <$> addr)
-    <|> const4 0x1 *> (jp <$> addr)
-    <|> const4 0x2 *> (call <$> addr)
-    <|> const4 0x3 *> (se <$> reg <*> byte)
-    <|> const4 0x4 *> (sne <$> reg <*> byte)
-    <|> const4 0x5 *> (sev <$> reg <*> reg) <* const4 0x0
-    <|> const4 0x6 *> (ld <$> reg <*> byte)
-    <|> const4 0x7 *> (add <$> reg <*> byte)
+    <|> const4 0x1 *> (jpAddr <$> addr)
+    <|> const4 0x2 *> (callAddr <$> addr)
+    <|> const4 0x3 *> (seRegByte <$> reg <*> byte)
+    <|> const4 0x4 *> (sneRegByte <$> reg <*> byte)
+    <|> const4 0x5 *> (seRegReg <$> reg <*> reg) <* const4 0x0
+    <|> const4 0x6 *> (ldRegByte <$> reg <*> byte)
+    <|> const4 0x7 *> (addRegByte <$> reg <*> byte)
+    <|> const4 0x8 *> (ldRegReg <$> reg <*> reg) <* const4 0x0
+    <|> const4 0x8 *> (orRegReg <$> reg <*> reg) <* const4 0x1
+    <|> const4 0x8 *> (andRegReg <$> reg <*> reg) <* const4 0x2
+    <|> const4 0x8 *> (xorRegReg <$> reg <*> reg) <* const4 0x3
+    <|> const4 0x8 *> (addRegReg <$> reg <*> reg) <* const4 0x4
+    <|> const4 0x8 *> (subRegReg <$> reg <*> reg) <* const4 0x5
+    <|> const4 0x8 *> (shr <$> reg <*> reg) <* const4 0x6
+    <|> const4 0x8 *> (subnRegReg <$> reg <*> reg) <* const4 0x7
+    <|> const4 0x8 *> (shl <$> reg <*> reg) <* const4 0xE
+    <|> const4 0x9 *> (sneRegReg <$> reg <*> reg) <* const4 0x0
+    <|> const4 0xA *> (ldIAddr <$> addr)
+    <|> const4 0xB *> (jpV0Addr <$> addr)
+    <|> const4 0xC *> (rnd <$> reg <*> byte)
+    <|> const4 0xD *> (drw <$> reg <*> reg <*> nibble)
+    <|> const4 0xE *> (skp <$> reg) <* const8 0x9E
+    <|> const4 0xE *> (sknp <$> reg) <* const8 0xA1
+    <|> const4 0xF *> (ldRegDT <$> reg) <* const8 0x07
+    <|> const4 0xF *> (ldRegKey <$> reg) <* const8 0x0A
+    <|> const4 0xF *> (ldDTReg <$> reg) <* const8 0x15
+    <|> const4 0xF *> (ldSTReg <$> reg) <* const8 0x18
+    <|> const4 0xF *> (addIReg <$> reg) <* const8 0x1E
+    <|> const4 0xF *> (ldFReg <$> reg) <* const8 0x29
+    <|> const4 0xF *> (ldBReg <$> reg) <* const8 0x33
+    <|> const4 0xF *> (ldMemRegs <$> reg) <* const8 0x55
+    <|> const4 0xF *> (ldRegsMem <$> reg) <* const8 0x65
   where const4 = assertEqBits 4
         const8 = assertEqBits 8
         const16 = assertEqBits 16
         addr = getBits 12
-        reg = getBits 4
+        nibble = getBits 4
+        reg = nibble
         byte = getBits 8
 
 sys :: Word16 -> Instruction
-sys loc = Instruction (InstructionInfo "SYS" [loc]) exec
+sys addr = Instruction (InstructionInfo "SYS" [Addr addr]) exec
   where exec _ _ = Left $ CpuException "SYS instruction is not allowed"
 
 cls :: Instruction
@@ -89,46 +151,130 @@ ret :: Instruction
 ret = Instruction (InstructionInfo "RET" []) exec
   where exec = undefined
 
-jp :: Word16 -> Instruction
-jp loc = Instruction (InstructionInfo "JP" [loc]) exec
+jpAddr :: Word16 -> Instruction
+jpAddr addr = Instruction (InstructionInfo "JP" [Addr addr]) exec
   where exec = undefined
   
-call :: Word16 -> Instruction
-call loc = Instruction (InstructionInfo "CALL" [loc]) exec
+callAddr :: Word16 -> Instruction
+callAddr addr = Instruction (InstructionInfo  "CALL" [Addr addr]) exec
   where exec = undefined
   
-se :: Word16 -> Word16 -> Instruction
-se reg byte = Instruction (InstructionInfo "SE" [reg, byte]) exec
+seRegByte :: Word16 -> Word16 -> Instruction
+seRegByte reg byte = Instruction (InstructionInfo "SE" [Reg reg, Byte byte]) exec
   where exec = undefined
   
-sne :: Word16 -> Word16 -> Instruction
-sne reg byte = Instruction (InstructionInfo "SNE" [reg, byte]) exec
+sneRegByte :: Word16 -> Word16 -> Instruction
+sneRegByte reg byte = Instruction (InstructionInfo "SNE" [Reg reg, Byte byte]) exec
   where exec = undefined
 
-sev :: Word16 -> Word16 -> Instruction
-sev reg1 reg2 = Instruction (InstructionInfo "SEV" [reg1, reg2]) exec
+seRegReg :: Word16 -> Word16 -> Instruction
+seRegReg regx regy = Instruction (InstructionInfo "SE" [Reg regx, Reg regy]) exec
   where exec = undefined
   
-ld :: Word16 -> Word16 -> Instruction
-ld reg byte = Instruction (InstructionInfo "LD" [reg, byte]) exec
+ldRegByte :: Word16 -> Word16 -> Instruction
+ldRegByte reg byte = Instruction (InstructionInfo "LDI" [Reg reg, Byte byte]) exec
   where exec = undefined
 
-add :: Word16 -> Word16 -> Instruction
-add reg byte = Instruction (InstructionInfo "ADD" [reg, byte]) exec
+addRegByte :: Word16 -> Word16 -> Instruction
+addRegByte reg byte = Instruction (InstructionInfo "ADDI" [Reg reg, Byte byte]) exec
   where exec = undefined
   
-ldv :: Word16 -> Word16 -> Instruction
-ldv dest src = Instruction (InstructionInfo "LDV" [dest, src]) exec
+ldRegReg :: Word16 -> Word16 -> Instruction
+ldRegReg regx regy = Instruction (InstructionInfo "LD" [Reg regx, Reg regy]) exec
   where exec = undefined
   
-or :: Word16 -> Word16 -> Instruction
-or reg1 reg2 = Instruction (InstructionInfo "OR" [reg1, reg2]) exec
+orRegReg :: Word16 -> Word16 -> Instruction
+orRegReg regx regy = Instruction (InstructionInfo "OR" [Reg regx, Reg regy]) exec
   where exec = undefined
 
-and :: Word16 -> Word16 -> Instruction
-and reg1 reg2 = Instruction (InstructionInfo "AND" [reg1, reg2]) exec
+andRegReg :: Word16 -> Word16 -> Instruction
+andRegReg regx regy = Instruction (InstructionInfo "AND" [Reg regx, Reg regy]) exec
   where exec = undefined
 
-xor :: Word16 -> Word16 -> Instruction
-xor reg1 reg2 = Instruction (InstructionInfo "XOR" [reg1, reg2]) exec
+xorRegReg :: Word16 -> Word16 -> Instruction
+xorRegReg regx regy = Instruction (InstructionInfo "XOR" [Reg regx, Reg regy]) exec
+  where exec = undefined
+
+addRegReg :: Word16 -> Word16 -> Instruction
+addRegReg regx regy = Instruction (InstructionInfo "ADD" [Reg regx, Reg regy]) exec
+  where exec = undefined
+
+subRegReg :: Word16 -> Word16 -> Instruction
+subRegReg regx regy = Instruction (InstructionInfo "SUB" [Reg regx, Reg regy]) exec
+  where exec = undefined
+
+shr :: Word16 -> Word16 -> Instruction
+shr regx regy = Instruction (InstructionInfo "SHR" [Reg regx, Reg regy]) exec
+  where exec = undefined
+
+subnRegReg :: Word16 -> Word16 -> Instruction
+subnRegReg regx regy = Instruction (InstructionInfo "SUBN" [Reg regx, Reg regy]) exec
+  where exec = undefined
+
+shl :: Word16 -> Word16 -> Instruction
+shl regx regy = Instruction (InstructionInfo "SHL" [Reg regx, Reg regy]) exec
+  where exec = undefined
+
+sneRegReg :: Word16 -> Word16 -> Instruction
+sneRegReg regx regy = Instruction (InstructionInfo "SNE" [Reg regx, Reg regy]) exec
+  where exec = undefined
+
+ldIAddr :: Word16 -> Instruction
+ldIAddr addr = Instruction (InstructionInfo "LD" [RegI, Addr addr]) exec
+  where exec = undefined
+
+jpV0Addr :: Word16 -> Instruction
+jpV0Addr addr = Instruction (InstructionInfo "JP" [Reg 0, Addr addr]) exec
+  where exec = undefined
+
+rnd :: Word16 -> Word16 -> Instruction
+rnd reg mask = Instruction (InstructionInfo "RND" [Reg reg, Byte mask]) exec
+  where exec = undefined
+
+drw :: Word16 -> Word16 -> Word16 -> Instruction
+drw xreg yreg n  = Instruction (InstructionInfo "DRW" [Reg xreg, Reg yreg, Nibble n]) exec
+  where exec = undefined
+
+skp :: Word16 -> Instruction
+skp reg = Instruction (InstructionInfo "SKP" [Reg reg]) exec
+  where exec = undefined
+
+sknp :: Word16 -> Instruction
+sknp reg = Instruction (InstructionInfo "SKNP" [Reg reg]) exec
+  where exec = undefined
+
+ldRegDT :: Word16 -> Instruction
+ldRegDT reg = Instruction (InstructionInfo "LD" [Reg reg, DelayTimer]) exec
+  where exec = undefined
+
+ldRegKey :: Word16 -> Instruction
+ldRegKey reg = Instruction (InstructionInfo "LD" [Reg reg, Key]) exec
+  where exec = undefined
+
+ldDTReg :: Word16 -> Instruction
+ldDTReg reg = Instruction (InstructionInfo "LD" [DelayTimer, Reg reg]) exec
+  where exec = undefined
+
+ldSTReg :: Word16 -> Instruction
+ldSTReg reg = Instruction (InstructionInfo "LD" [SoundTimer, Reg reg]) exec
+  where exec = undefined
+
+addIReg :: Word16 -> Instruction
+addIReg reg = Instruction (InstructionInfo "ADD" [RegI, Reg reg]) exec
+  where exec = undefined
+
+ldFReg :: Word16 -> Instruction
+ldFReg reg = Instruction (InstructionInfo "LD" [SpriteLoc, Reg reg]) exec
+  where exec = undefined
+
+ldBReg :: Word16 -> Instruction
+ldBReg reg = Instruction (InstructionInfo "LD" [BCD, Reg reg]) exec
+  where exec = undefined
+
+ldMemRegs :: Word16 -> Instruction
+ldMemRegs x = Instruction (InstructionInfo "LD" [AddrI, Reg x]) exec
+  where exec = undefined
+
+ldRegsMem :: Word16 -> Instruction
+ldRegsMem x = Instruction (InstructionInfo "LD" [Reg x, AddrI]) exec
   where exec = undefined
---
+++ test/Hip8/InstructionSpec.hs
@@ -0,0 +1,87 @@
+module Hip8.InstructionSpec (spec) where
+
+import Test.Hspec
+import Test.Hspec.QuickCheck
+import Test.QuickCheck
+import Hip8.Instruction
+import Data.Maybe
+
+spec :: Spec
+spec = do
+  describe "parseInstruction" $ do
+    it "parses SYS addr" $
+      parseInstruction 0x0ABC `shouldBe` Just (sys 0xABC)
+    it "parses CLS" $
+      parseInstruction 0x00E0 `shouldBe` Just cls
+    it "parses RET" $
+      parseInstruction 0x00EE `shouldBe` Just ret
+    it "parses JP addr" $
+      parseInstruction 0x1ABC `shouldBe` Just (jpAddr 0xABC)
+    it "parses CALL addr" $
+      parseInstruction 0x2ABC `shouldBe` Just (callAddr 0xABC)
+    it "parses SE Vx, byte" $
+      parseInstruction 0x3ABC `shouldBe` Just (seRegByte 0xA 0xBC)
+    it "parses SNE Vx, byte" $
+      parseInstruction 0x4ABC `shouldBe` Just (sneRegByte 0xA 0xBC)
+    it "parses SE Vx, Vy" $
+      parseInstruction 0x5AB0 `shouldBe` Just (seRegReg 0xA 0xB)
+    it "parses LD Vx, byte" $
+      parseInstruction 0x6ABC `shouldBe` Just (ldRegByte 0xA 0xBC)
+    it "parses ADD Vx, byte" $
+      parseInstruction 0x7ABC `shouldBe` Just (addRegByte 0xA 0xBC)
+    it "parses LD Vx, Vy" $
+      parseInstruction 0x8AB0 `shouldBe` Just (ldRegReg 0xA 0xB)
+    it "parses OR Vx, Vy" $
+      parseInstruction 0x8AB1 `shouldBe` Just (orRegReg 0xA 0xB)
+    it "parses AND Vx, Vy" $
+      parseInstruction 0x8AB2 `shouldBe` Just (andRegReg 0xA 0xB)
+    it "parses XOR Vx, Vy" $
+      parseInstruction 0x8AB3 `shouldBe` Just (xorRegReg 0xA 0xB)
+    it "parses ADD Vx, Vy" $
+      parseInstruction 0x8AB4 `shouldBe` Just (addRegReg 0xA 0xB)
+    it "parses SUB Vx, Vy" $
+      parseInstruction 0x8AB5 `shouldBe` Just (subRegReg 0xA 0xB)
+    it "parses SHR Vx, Vy" $
+      parseInstruction 0x8AB6 `shouldBe` Just (shr 0xA 0xB)
+    it "parses SUBN Vx, Vy" $
+      parseInstruction 0x8AB7 `shouldBe` Just (subnRegReg 0xA 0xB)
+    it "parses SHL Vx, Vy" $
+      parseInstruction 0x8ABE `shouldBe` Just (shl 0xA 0xB)
+    it "parses SNE Vx, Vy" $
+      parseInstruction 0x9AB0 `shouldBe` Just (sneRegReg 0xA 0xB)
+    it "parses LD I, addr" $
+      parseInstruction 0xAABC `shouldBe` Just (ldIAddr 0xABC)
+    it "parses JP V0, addr" $
+      parseInstruction 0xBABC `shouldBe` Just (jpV0Addr 0xABC)
+    it "parses RND Vx, byte" $
+      parseInstruction 0xCABC `shouldBe` Just (rnd 0xA 0xBC)
+    it "parses DRW Vx, Vy, nibble" $
+      parseInstruction 0xDABC `shouldBe` Just (drw 0xA 0xB 0xC)
+    it "parses SKP Vx" $
+      parseInstruction 0xEA9E `shouldBe` Just (skp 0xA)
+    it "parses SKNP Vx" $
+      parseInstruction 0xEAA1 `shouldBe` Just (sknp 0xA)
+    it "parses LD Vx, DT" $
+      parseInstruction 0xFA07 `shouldBe` Just (ldRegDT 0xA)
+    it "parses LD Vx, K" $
+      parseInstruction 0xFA0A `shouldBe` Just (ldRegKey 0xA)
+    it "parses LD DT, Vx" $
+      parseInstruction 0xFA15 `shouldBe` Just (ldDTReg 0xA)
+    it "parses LD ST, Vx" $
+      parseInstruction 0xFA18 `shouldBe` Just (ldSTReg 0xA)
+    it "parses ADD I, Vx" $
+      parseInstruction 0xFA1E `shouldBe` Just (addIReg 0xA)
+    it "parses LD F, Vx" $
+      parseInstruction 0xFA29 `shouldBe` Just (ldFReg 0xA)
+    it "parses LD B, Vx" $
+      parseInstruction 0xFA33 `shouldBe` Just (ldBReg 0xA)
+    it "parses LD [I], Vx" $
+      parseInstruction 0xFA55 `shouldBe` Just (ldMemRegs 0xA)
+    it "parses LD Vx, [I]" $
+      parseInstruction 0xFA65 `shouldBe` Just (ldRegsMem 0xA)
+      
+    prop "never parses different words to same instruction" $
+      \w1 w2 -> (w1 /= w2) ==>
+        let i1 = parseInstruction w1
+            i2 = parseInstruction w2
+        in isNothing i1 || i1 /= i2
